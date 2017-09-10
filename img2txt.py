9340f96102eac008bb6f97b038b79aa61c31c603
yukikawana <kojirou.tensou@gmail.com>
2017-09-10 17:35:58 +0900

    ha

--- img2txt.py
+++ img2txt.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageDraw, ImageChops
 import numpy as np
 from numpy import linalg
 from extra import colortrans
+import sys
 
 import pdb
 import os
@@ -51,40 +52,38 @@ def main():
     imr = im.resize((blockwidth * respb, blockheight * respb), Image.ANTIALIAS)
     print imr.size
     start = 50
-    part = imr.crop((start+8,start,start+8+respb/2,start+respb))
-    print part.size
-    part.save('original.png')
-    imgq = part.quantize(2, kmeans=True)
-    imgq.save('bicolor.png')
-    default_palette =  getPaletteInRgb(imgq)
-    bg = hex(default_palette[0])[2:4]+hex(default_palette[1])[2:4]+hex(default_palette[2])[2:4]
-    fg = hex(default_palette[3])[2:4]+hex(default_palette[4])[2:4]+hex(default_palette[5])[2:4]
-    print default_palette
-    """
-    default_palette = [
-    255,0,0,
-    0,0,255
-    ]
-    """
-    count = 0
-    imgs = create_temp_imgs(count, default_palette)
-    new_palette = [
-    0,0,255,
-    255,0,0
-    ]
-    print len(imgs)
-    count2 = len(imgs)
-    print count2
-    #pdb.set_trace()
-    diffs = np.zeros([1,25])
-    for img in imgs:
-        imgnp = np.asarray(img)
-        imgqnp = np.asarray(imgq)
-        diffs[0,count] = linalg.norm(imgnp-imgqnp)
-        count+=1
-    bgshort,rgb = colortrans.rgb2short(bg)
-    fgshort,rgb = colortrans.rgb2short(fg)
-    print '\033[38;5;{0};48;5;{1}m'.format(fgshort,bgshort)+unicodes[np.argmin(diffs)]+'\033[0;00m'
+    for hh in xrange(blockheight):
+        for ww in xrange(blockwidth):
+            part = imr.crop((ww*respb/2,hh*respb,(ww+1)*respb/2,(hh+1)*respb))
+            imgq = part.quantize(2, kmeans=True)
+            default_palette =  getPaletteInRgb(imgq)
+            bg = hex(default_palette[0])[2:4].zfill(2)+hex(default_palette[1])[2:4].zfill(2)+hex(default_palette[2])[2:4].zfill(2)
+            fg = hex(default_palette[3])[2:4].zfill(2)+hex(default_palette[4])[2:4].zfill(2)+hex(default_palette[5])[2:4].zfill(2)
+            """
+            default_palette = [
+            255,0,0,
+            0,0,255
+            ]
+            """
+            count = 0
+            imgs = create_temp_imgs(count, default_palette)
+            new_palette = [
+            0,0,255,
+            255,0,0
+            ]
+            count2 = len(imgs)
+            #pdb.set_trace()
+            diffs = np.zeros([1,25])
+            for img in imgs:
+                imgnp = np.asarray(img)
+                imgqnp = np.asarray(imgq)
+                diffs[0,count] = linalg.norm(imgnp-imgqnp)
+                count+=1
+            bgshort,rgb = colortrans.rgb2short(bg)
+            fgshort,rgb = colortrans.rgb2short(fg)
+            
+            sys.stdout.write('\033[38;5;{0};48;5;{1}m'.format(fgshort,bgshort)+unicodes[np.argmin(diffs)]+'\033[0;00m')
+        sys.stdout.write('\n')
 
 
 
@@ -99,7 +98,6 @@ def create_temp_imgs(count,default_palette):
         d.rectangle([0, 0, w, 15], fill=1)
         count+=1
         tempimgs.append(temp)
-        temp.save('{}.png'.format(count))
 
     for h in xrange(1,respb/2):
         temp = Image.new('P', (respb/2,respb), 0)
@@ -108,7 +106,6 @@ def create_temp_imgs(count,default_palette):
         d.rectangle([0, 15-h*2+1, respb/2-1, 15], fill=1)
         count+=1
         tempimgs.append(temp)
-        temp.save('{}.png'.format(count))
 
     #quarter blocks
     for qw in [0,respb/4]:
@@ -119,7 +116,6 @@ def create_temp_imgs(count,default_palette):
             d.rectangle([qw, qh, qw+respb/4-1, respb/2-1+qh], fill=1)
             count+=1
             tempimgs.append(temp)
-            temp.save('{}.png'.format(count))
 
     #quarter diagonal two blocks
     for qwqh in [ [[0,0],[respb/4,respb/2]], [[0,respb/2],[respb/4,0]]]:
@@ -130,7 +126,6 @@ def create_temp_imgs(count,default_palette):
             d.rectangle([qw, qh, qw+respb/4-1, respb/2-1+qh], fill=1)
         count+=1
         tempimgs.append(temp)
-        temp.save('{}.png'.format(count))
 
     #three blocks
     for qwqh in [ [[0,0],[respb/4,respb/2],[respb/4,0]], [[0,0],[respb/4,respb/2],[0,respb/2]], [[0,respb/2],[respb/4,0],[0,0]],[[0,respb/2],[respb/4,0],[respb/4,respb/2]]]:
@@ -141,7 +136,6 @@ def create_temp_imgs(count,default_palette):
             d.rectangle([qw, qh, qw+respb/4-1, respb/2-1+qh], fill=1)
         count+=1
         tempimgs.append(temp)
-        temp.save('{}.png'.format(count))
 
     return tempimgs
 
